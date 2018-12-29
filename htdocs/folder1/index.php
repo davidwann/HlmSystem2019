ca05485bae9afbb00abc087a5eeed58064add25f
David Wann <david.wann@gmail.com>
2018-12-29 16:53:28 -0600

    first commit

---
+++ htdocs/folder1/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <title>Folder1 PHP Page</title>
+</head>
+<body>
+
+<!--  index.php  -->
+
+<h2>Folder1 Index Page</h2>
+
+
+<p>&nbsp;</p>
+
+
+<?php
+
+echo "This text was echoed by PHP!";
+
+?>
+
+
+
+
+
+<p>&nbsp;</p>
+
+
+</body>
+</html>
---
+++ htdocs/index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+  <title>My PHP Page</title>
+</head>
+<body>
+
+<!--  index.php  -->
+
+<h2>HLM with Bitnami</h2>
+
+
+<p>&nbsp;</p>
+
+
+<?php
+
+echo "This text was echoed by PHP!";
+
+?>
+
+
+
+
+
+<p>&nbsp;</p>
+
+
+</body>
+</html>
+
